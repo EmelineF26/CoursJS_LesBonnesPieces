fcbac32091baf1a6c69f61ebce52bf0206d67fa1
Emeline Farget <emeline.farget@gmail.com>
2024-12-13 16:26:44 +0100

    Suite exercice 1

--- pieces.js
+++ pieces.js
@@ -1,35 +1,39 @@
-// On récupère nos pièces depuis le fichier JSON
-const reponse = await fetch("pieces-autos.json");
+// On récupère les pièces depuis le fichier JSON
+const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
 
-// On crée des balises
-const article = pieces[0];
+for (let i = 0; i < pieces.length; i++) {
 
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
+    const article = pieces[i];
+    // On récupère l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // On crée une balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+    // On crée des balises 
+    const imageElement = document.createElement("img");
+    imageElement.src = article.image;
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = article.nom;
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+    const stockElement = document.createElement("p");
+    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+    
+    // On rattache la balise article a la section Fiches
+    sectionFiches.appendChild(pieceElement);
 
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
+    // On rattache l’image à pieceElement (la balise article)
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
 
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+    // On ajoute des éléments au DOM pour l'exercice
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(stockElement);
 
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
-
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
-
-const stockElement = document.createElement("p");
-stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
-
-// On rattache nos balises au DOM
-const sectionFiches = document.querySelector(".fiches");
-
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(stockElement);
+}
